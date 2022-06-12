3347a56cc1ed435458a94e8d4f016e3dc1006bf6
yaahsin <brillanteoboe@gmail.com>
2022-06-12 16:59:56 +0800

    feat: add user model

--- models/User.js
+++ models/User.js
@@ -4,14 +4,22 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
-  id: {
-    type: Number,
-    required: true
-  },
   name: {
     type: String, // 資料型別是字串
     required: true // 這是個必填欄位
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
   }
 })
 
-module.exports = mongoose.model('User', todoSchema)
+module.exports = mongoose.model('User', userSchema)
