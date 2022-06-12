b0c35209b8500b8a884e9a668f0da9d5756ba731
yaahsin <brillanteoboe@gmail.com>
2022-06-12 17:20:59 +0800

    feat: add user registration feature

--- models/User.js
+++ models/User.js
@@ -1,12 +1,11 @@
-// models/cost
-
+// models/user
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
   name: {
-    type: String, // 資料型別是字串
-    required: true // 這是個必填欄位
+    type: String,
+    required: true
   },
   email: {
     type: String,
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -4,6 +4,7 @@
 const express = require('express')
 const router = express.Router()
 const Cost = require('../../models/cost')
+const User = require('../../models/user')
 
 // 區塊2: 引入路由模組
 router.get('/login', (req, res) => {
@@ -15,6 +16,34 @@ router.post('/login', (req, res) => {
 
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  // 取得註冊表單參數
+  const { name, email, password, confirmPassword } = req.body
+  // 檢查使用者是否已經註冊
+  User.findOne({ email }).then(user => {
+    // 如果已經註冊：退回原本畫面
+    if (user) {
+      console.log('User already exists.')
+      res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    } else {
+      // 如果還沒註冊：寫入資料庫
+      return User.create({
+        name,
+        email,
+        password
+      })
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
+    }
+  })
+    .catch(err => console.log(err))
 })
 
 // 區塊3: 匯出路由器
