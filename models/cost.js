5cd72ee0b608518736a5c09324198da7f98f06da
yaahsin <brillanteoboe@gmail.com>
2022-06-18 13:15:28 +0800

    reupload files

---
+++ models/cost.js
@@ -0,0 +1,48 @@
+// models/cost
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const costSchema = new Schema({
+  id: {
+    type: Number
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  userId: {  // 加入關聯設定
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    index: true,
+    required: true
+  },
+  category: {
+    type: Schema.Types.String,
+    ref: 'Category',
+    index: true,
+    required: true
+  },
+  categoryId: {  // 加入關聯設定
+    type: Schema.Types.ObjectId,
+    ref: 'Category',
+    index: true,
+    required: true
+  },
+  icon: {  // 加入關聯設定
+    type: Schema.Types.String,
+    ref: 'Category',
+    index: true,
+    required: true
+  },
+})
+
+module.exports = mongoose.model('Cost', costSchema)
